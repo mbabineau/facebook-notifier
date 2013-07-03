e910c83adc11b3929ce82d300c7249bc6d349a46
Mike Babineau <michael.babineau@gmail.com>
2013-07-02 17:35:09 -0700

    Better output, fixes for 50k+ ID runs

--- README.md
+++ README.md
@@ -20,8 +20,9 @@ Usage
 ------------
 
 ```
-usage: fb_notifier.py [-h] -f RECIPIENTS_FILE [-c MAX_CONCURRENT] -a
-                      ACCESS_TOKEN -t TEMPLATE [-r REF] [-u HREF]
+usage: fb_notifier.py [-h] -f RECIPIENTS_FILE [-c MAX_CONCURRENT]
+                      [-v [VERBOSE]] [-q [QUIET]] -a ACCESS_TOKEN -t TEMPLATE
+                      [-r REF] [-u HREF]
 
 Sends a Facebook notification to each of a list of FB users. See
 https://developers.facebook.com/docs/concepts/notifications/ for more
@@ -35,7 +36,12 @@ Notifier arguments:
                         File containing list of recipients (newline-delimited
                         FB IDs)
   -c MAX_CONCURRENT, --max-concurrent MAX_CONCURRENT
-                        Maximum number of concurrent requests
+                        Maximum number of concurrent requests (default: 10,
+                        max: 100)
+  -v [VERBOSE], --verbose [VERBOSE]
+                        Show all results (instead of just failures)
+  -q [QUIET], --quiet [QUIET]
+                        Show no results
 
 Facebook arguments:
   -a ACCESS_TOKEN, --access-token ACCESS_TOKEN
--- fb_notifier.py
+++ fb_notifier.py
@@ -5,11 +5,22 @@ import grequests
 
 BASE_FB_URL = 'https://graph.facebook.com'
 
+def chunks(l, n):
+    '''Split l into n-sized chunks'''
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+def parse_id(url):
+    '''Recover the FB ID from the Notifications URL'''
+    return url.split('/')[3]
+
 def main():
     parser = argparse.ArgumentParser(description='Sends a Facebook notification to each of a list of FB users. See https://developers.facebook.com/docs/concepts/notifications/ for more information.')
     notifier_args = parser.add_argument_group('Notifier arguments')
     notifier_args.add_argument('-f', '--recipients-file', type=argparse.FileType('rt'), required=True, help='File containing list of recipients (newline-delimited FB IDs)')
-    notifier_args.add_argument('-c', '--max-concurrent', default=10, help='Maximum number of concurrent requests')
+    notifier_args.add_argument('-c', '--max-concurrent', default=10, type=int, help='Maximum number of concurrent requests (default: 10, max: 100)')
+    notifier_args.add_argument('-v', '--verbose', const=True, default=False, nargs='?', help='Show all results (instead of just failures)')
+    notifier_args.add_argument('-q', '--quiet', const=True, default=False, nargs='?', help='Show no results')
     
     # Facebook parameters    
     fb_args = parser.add_argument_group('Facebook arguments')            
@@ -22,14 +33,22 @@ def main():
 
     user_ids = args.recipients_file.read().splitlines()
 
-    rs = (grequests.post("%s/%s/notifications" % (BASE_FB_URL, user_id), params={
+    rs = [grequests.post("%s/%s/notifications" % (BASE_FB_URL, user_id), params={
             'access_token': args.access_token,
             'template': args.template,
             'ref': args.ref,
             'href': args.href
-        }) for user_id in user_ids)
+        }) for user_id in user_ids]
 
-    print grequests.map(rs, size=args.max_concurrent)
+    chunk_size = args.max_concurrent if args.max_concurrent > 100 else 100
+    for s in chunks(rs, chunk_size):
+        responses = grequests.map(s, size=args.max_concurrent)
+        for r in responses:
+            try:
+                if not args.quiet and (args.verbose or r.status_code >= 300): 
+                    print (r.status_code, parse_id(r.url), r.text)
+            finally:
+                r.raw.release_conn()
 
 if __name__ == "__main__":
     main()
